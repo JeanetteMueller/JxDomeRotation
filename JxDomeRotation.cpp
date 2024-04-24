126fab28e2599b6b712e6ec942f01174764d3849
Jeanette Müller <themaverick@themaverick.de>
2024-04-24 13:10:23 +0200

    enable dome rotation via PWM servo shield

--- JxDomeRotation.cpp
+++ JxDomeRotation.cpp
@@ -11,36 +11,90 @@ JxDomeRotation::JxDomeRotation(int16_t inputMin, int16_t inputMax, int16_t input
 
 void JxDomeRotation::setupMotor(MODE mode, uint8_t pin1, uint8_t pin2)
 {
-    _motor = new CytronMD(mode, pin1, pin2);
-    _speed = 0;
-    _motor->setSpeed(_speed);
+    if (_isReady != true)
+    {
+        _motor = new CytronMD(mode, pin1, pin2);
+        _speed = 0;
+        _motor->setSpeed(_speed);
+
+        _currentMotorType = Direct;
+        _isReady = true;
+    }
 }
 
-void JxDomeRotation::setCurrentPositionAsFront(){
+void JxDomeRotation::setupMotor(Adafruit_PWMServoDriver pwm, uint8_t pinDir, uint8_t pinPwm)
+{
+    if (_isReady != true)
+    {
+        _pwm = pwm;
+        _pinDir = pinDir;
+        _pinPwm = pinPwm;
 
+        _currentMotorType = PWMServoDriver;
+        _isReady = true;
+    }
 }
 
 void JxDomeRotation::updateMotorWith(int16_t value, uint16_t deadPoint, int16_t maxSpeed)
 {
-    if (value >= _inputMin && value <= _inputMax)
+    if (value >= _inputCenter + deadPoint || value <= _inputCenter - deadPoint)
     {
-        int16_t rotationSpeed = map(value, _inputMin, _inputMax, maxSpeed, -(maxSpeed));
+        if (_currentMotorType == Direct)
+        {
+            // update via Motordriver pins
 
-        _motor->setSpeed(rotationSpeed);
+            if (value >= _inputCenter + deadPoint)
+            {
+                int16_t rotationSpeed = map(value - deadPoint, _inputCenter, _inputMax - deadPoint, maxSpeed, -(maxSpeed));
+
+                _motor->setSpeed(rotationSpeed);
+            }
+            else if (value <= _inputCenter - deadPoint)
+            {
+                int16_t rotationSpeed = map(value + deadPoint, _inputMin + deadPoint, _inputCenter, maxSpeed, -(maxSpeed));
+
+                _motor->setSpeed(rotationSpeed);
+            }
+        }
+        else if (_currentMotorType == PWMServoDriver)
+        {
+            // update via PWM
+            if (value > _inputCenter)
+            {
+                uint16_t rotationSpeed = map(value, _inputCenter, _inputMax, 0, 4095);
+
+                _pwm.setPWM(_pinDir, 0, 4095);
+                _pwm.setPWM(_pinPwm, 0, rotationSpeed);
+            }
+            else if (value < _inputCenter)
+            {
+                uint16_t rotationSpeed = map(value, _inputMin, _inputCenter, 4095, 0);
+
+                _pwm.setPWM(_pinDir, 0, 0);
+                _pwm.setPWM(_pinPwm, 0, rotationSpeed);
+            }
+        }
     }
     else
     {
-        stop();
+        if (_currentMotorType == Direct)
+        {
+            _motor->setSpeed(_speed);
+        }
+        else if (_currentMotorType == PWMServoDriver)
+        {
+            _pwm.setPWM(_pinDir, 0, 0);
+            _pwm.setPWM(_pinPwm, 0, 0);
+        }
     }
+}
+
+int16_t JxDomeRotation::getCurrentSpeed()
+{
+    return _speed;
 }
 
 void JxDomeRotation::stop()
 {
-    _speed = 0;
-    updateSpeed();
-}
-
-void JxDomeRotation::updateSpeed()
-{
-    _motor->setSpeed(_speed);
+    updateMotorWith(0, 0);
 }
--- JxDomeRotation.h
+++ JxDomeRotation.h
@@ -4,27 +4,43 @@
 
 #include "Arduino.h"
 #include <CytronMotorDriver.h>
+#include <Adafruit_PWMServoDriver.h>
 
 class JxDomeRotation
 {
 public:
+
+    enum MotorType {
+        Direct, PWMServoDriver
+    };
+    
     int16_t currentSpeed() const { return _speed; };
 
     JxDomeRotation(int16_t inputMin, int16_t inputMax, int16_t inputCenter, uint32_t analogFrequenc = 18000);
 
     void setupMotor(MODE mode, uint8_t pin1, uint8_t pin2);
+    void setupMotor(Adafruit_PWMServoDriver pwm, uint8_t pinDir, uint8_t pinPwm);
 
-    void setCurrentPositionAsFront();
     void updateMotorWith(int16_t value, uint16_t deadPoint, int16_t maxSpeed = 255);
+    int16_t getCurrentSpeed();
     void stop();
 
 private:
+    bool _isReady = false;
+    MotorType _currentMotorType;
+
+    int16_t _speed = 0;
+
     int16_t _inputMin = -500;
     int16_t _inputMax = 500;
     int16_t _inputCenter = 0;
-    int16_t _speed = 0;
+    
 
     CytronMD *_motor;
+
+    Adafruit_PWMServoDriver _pwm;
+    uint8_t _pinDir;
+    uint8_t _pinPwm;
 
     void updateSpeed();
 };
